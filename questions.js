ae45db719b82cbb2cf184cac4850545af44d3492
Ananas <mrs.anna.noga@gmail.com>
2023-01-13 19:16:35 +0000

    question.js is filled with questions, answers and correct answers

---
+++ questions.js
@@ -0,0 +1,51 @@
+var quizQuestions = [
+    {
+        question: "What is a correct syntax to output 'Hello World' in Java?",
+        answers: [
+            "Console.WriteLine('Hello World')",
+            "print('Hello World')",
+            "echo('Hello World')",
+            "System.out.println('Hello World')"
+        ],
+        correctAnswerIndex: 3
+    },
+    {
+        question: "How do you insert COMMENTS in Java code",
+        answers: [
+            "// This is a comment",
+            "/* This is a comment",
+            "# This is a comment"
+        ],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Which data type is used to create a variable that should store text?",
+        answers: [
+            "Txt",
+            "String",
+            "myString",
+            "string"
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "Which method can be used to find the length of a string?",
+        answers: [
+            "getLength()",
+            "len()",
+            "getSize()",
+            "length()"
+        ],
+        correctAnswerIndex: 3
+    },
+    {
+        question: "Which method can be used to return a string in upper case letters?",
+        answers: [
+            "upperCase()",
+            "tuc()",
+            "toUpperCase()",
+            "touppercase()"
+        ],
+        correctAnswerIndex: 2
+    },
+]
